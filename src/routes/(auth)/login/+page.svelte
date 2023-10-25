8e4d5a70f62c51f1638133ed366c45548bfab7b5
3b00d09 <bd315977@gmail.com>
2023-10-25 19:21:07 +0100

    added login page layout

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,87 @@
+<div class="container">
+    <div class="header">
+        <h1>Log in</h1>
+        <p><a href="/register">New user? Register</a></p>
+    </div>
+    <form method="post" class="form">
+        <label for="username">Username</label>
+        <input required type="text" name="username" placeholder="anon64942495"/>
+        <label for="password">Password</label>
+        <input required type="password" name="password" placeholder="*********"/>
+        <button type="submit">Log In</button>
+        <div class="footer-links">
+            <p><a href="/">Forgot Username</a></p>
+            <p><a href="/">Reset Password</a></p>
+        </div>
+    </form>
+</div>
+
+<style>
+
+.container {
+    display: grid;
+    place-items: center;
+    height: 100%;
+    border: 1px solid hsl(240, 3.7%, 15.9%);
+    border-radius: 0.5rem;
+    width: 50%;
+    justify-self: center;
+}
+
+.header {
+    place-self: start;
+    padding: 2rem;
+}
+
+
+.form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 75%;
+    padding: 0.5rem;
+}
+
+.footer-links {
+    display: flex;
+    justify-content: space-around;
+    gap: 0.5rem;
+}
+
+h1{
+    font-size: 1.5rem;
+    line-height: 2rem;
+    letter-spacing: -0.025em;
+}
+
+input {
+    padding: 1rem;
+    border: 1px solid white;
+    border-radius: 0.5rem;
+    background-color: transparent;
+    font-family: inherit;
+    font-size: inherit;
+    color: inherit;
+}
+
+p {
+    color: hsl(240, 5%, 64.9%);
+    font-size: 0.875rem;
+    margin-top: 0.5rem;
+}
+
+a{
+    color: inherit;
+    text-decoration: inherit;
+}
+
+button {
+    border-radius: 0.5rem;
+    background-color: aliceblue;
+    color: black;
+    padding: 0.5rem;
+    font-family: inherit;
+    font-size: inherit;
+}
+
+</style>
---
+++ src/routes/(auth)/register/+page.svelte
@@ -0,0 +1,13 @@
+<div class="auth-container">
+    <div class="auth-image">
+        <img src="/images/auth-bg.jpg" alt="man holding phone">
+    </div>
+</div>
+
+
+<style>
+    .auth-container{
+        display: flex;
+        height: 100vh;
+    }
+</style>
