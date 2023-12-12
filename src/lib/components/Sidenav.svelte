3213308e3688a7c33aa47ab47c330e2f6ccb6260
3b00d09 <bd315977@gmail.com>
2023-12-12 18:02:12 +0000

    sidebar css

--- src/lib/components/Sidenav.svelte
+++ src/lib/components/Sidenav.svelte
@@ -19,6 +19,7 @@
     <li class="icon-container"><button on:click={setActiveIcon}><img src="/images/icons/search.png" alt="Home Icon"/></button></li>
     <li class="icon-container"><button on:click={setActiveIcon}><img src="/images/icons/inbox.png" alt="Home Icon"/></button></li>
     <li class="icon-container"><button on:click={setActiveIcon}><img src="/images/icons/profile.png" alt="Home Icon"/></button></li>
+    <li class="icon-container"><button on:click={setActiveIcon}><img src="/images/icons/settings.svg" alt="Settings Icon"/></button></li>
 </ul>
 
 
@@ -26,24 +27,33 @@
 
     ul{
         list-style: none;
-        display: flex;
-        flex-direction: column;
-        gap: 3rem;
+        display: grid;
         text-align: center;
         background: transparent;
         border-radius: 8px;
         padding: 1rem 1.75rem;
         box-shadow: -2px -2px 6px -4px rgba(226, 224, 224, 0.5), 2px 2px 6px 4px rgba(0, 0, 0, 0.5);
+        height: 70vh;
+        grid-template-columns: repeat(1, 3rem);
+        grid-template-rows: repeat(4, auto) 1fr;
+        justify-items: center;
+
+        grid-auto-rows: min-content;
+        gap: 2rem;
+        /** prevents the active::before from exploding*/
+        align-items: baseline;
+    }
+
+    ul li:nth-child(5){
+        align-self: end;
     }
 
     button{
         padding: .45rem;
-        width: 3rem;
-        height: 3rem;
         border: none;
     }
 
-    li > button > img{
+    button > img{
         width: 100%;
     }
 
@@ -66,7 +76,6 @@
         background-color: var(--action);
         filter: drop-shadow(0px 0px 5px rgba(108, 92, 214, 0.50));
         width: 1px;
-        height: 100%;
     }
 
 </style>
