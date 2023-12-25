94d876776c07578c9268fef1b28666f3e492d01f
Diyara <diyararashid@gmail.com>
2023-12-25 03:48:44 +0100

    Styling Search and fixing profile images

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -2,12 +2,13 @@
   import type { usersTable } from "$lib/server/schema";
 
   type User = typeof usersTable.$inferSelect;
-
+  let isSearching = false;
   let searchTerm = "";
   let searchResults: User[] = [];
 
   const searchUsers = async () => {
-    if (!searchTerm.trim()) {
+    isSearching = !!searchTerm.trim();
+    if (!isSearching) {
       searchResults = [];
       return;
     }
@@ -17,61 +18,123 @@
     );
     if (response.ok) {
       searchResults = await response.json();
+      isSearching = searchResults.length > 0;
     }
   };
 </script>
+<div class="app">
+  <div class="homepage-content {isSearching ? 'blur' : ''}">
 
-<form on:submit|preventDefault={searchUsers}>
-  <input
-    type="text"
-    bind:value={searchTerm}
-    placeholder="Search users..."
-    on:input={searchUsers}
-    class="search-input"
-  />
-</form>
-<div class="search-bar">
-  {#if searchResults.length > 0}
-    {#each searchResults as user}
-      <li class="user-item">
-        <span>{user.username}</span>
-      </li>
-    {/each}
-  {/if}
+  </div>
+
+  <div class="search-container">
+    <form on:submit|preventDefault={searchUsers} class="search-form">
+      <input
+        type="text"
+        bind:value={searchTerm}
+        placeholder="Search users..."
+        on:input={searchUsers}
+        class="search-input"
+      />
+    </form>
+
+    <div class="search-results {isSearching ? 'active' : ''}">
+      {#if searchResults.length > 0}
+        <ul>
+          {#each searchResults as user}
+            <li class="user-item">
+              <img src={user.profilePictureUrl} alt={`${user.username}'s profile picture`} class="user-image">
+              <span>{user.username}</span>
+            </li>
+          {/each}
+        </ul>
+      {/if}
+    </div>
+  </div>
 </div>
-
 <style>
-  form {
-    width: 100%;
-    display: grid;
-  }
+ .app {
+  position: relative;
+}
 
-  .search-bar {
-    display: flex;
-    align-items: center;
-  }
+.homepage-content.blur {
+  filter: blur(5px);
+  transition: filter 0.3s;
+}
 
-  .search-input {
-    padding: 0.75rem;
-    border-radius: 10px;
-    font-size: 0.65rem;
-    flex-grow: 1;
-    box-shadow:
-      -2px -2px 6px -4px rgba(226, 224, 224, 0.5) inset,
-      2px 2px 6px 4px rgba(0, 0, 0, 0.5) inset;
-    background: transparent;
-    border: none;
-    outline: none;
-    color: inherit;
-    font: inherit;
-  }
+.search-container {
+  position: relative; 
+  width: 300%;
+}
 
-  .user-item {
-    display: flex;
-    align-items: center;
-  }
+.search-form {
+  width: 100%; 
+  display: grid;
+}
 
-  .user-item span {
-    margin-left: 1rem;
-  }
+.search-input {
+  padding: 0.75rem;
+  border-radius: 10px;
+  font-size: 1rem;
+  width: 100%; 
+  box-shadow: -2px -2px 6px -4px rgba(226, 224, 224, 0.5) inset,
+              2px 2px 6px 4px rgba(0, 0, 0, 0.5) inset;
+  background: transparent;
+  border: none;
+  outline: none;
+  color: inherit;
+  font: inherit;
+}
+
+.search-results {
+  display: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  background: #131313;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+  max-height: 300px;
+  overflow-y: auto;
+  border-radius: 0 0 10px 10px;
+  z-index: 100;
+}
+
+.search-results.active {
+  display: block;
+}
+.user-item {
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  position: relative; 
+}
+
+.user-item::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 8px;
+  background: #131313; 
+  box-shadow: -5px -5px 5px 0px rgba(255, 255, 255, 0.05) inset, 5px 5px 5px 0px rgba(0, 0, 0, 0.50) inset;
+}
+
+.user-item:last-child::after {
+  content: none; 
+}
+
+.user-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+
+.user-item span {
+  margin-left: 1rem;
+}
+
+
 </style>
