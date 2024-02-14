84d8da0e4ba93c52ad8e2a8ee4e0781bd5a893dc
3b00d09 <bd315977@gmail.com>
2024-02-14 16:03:16 +0000

    cooking

--- src/routes/(app)/users/[username]/comments/+page.server.ts
+++ src/routes/(app)/users/[username]/comments/+page.server.ts
@@ -1,8 +1,8 @@
 import { dbClient } from '$lib/server/db.js';
-import { commentsTable, postsTable, usersTable } from '$lib/server/schema.js';
+import { commentsTable, likesCommentTable, likesPostTable, postsTable, usersTable } from '$lib/server/schema.js';
 import type { CommentWithProfileImage, PostWithProfile } from '$lib/types';
 import { redirect } from '@sveltejs/kit';
-import { eq, and, isNull, getTableColumns } from 'drizzle-orm';
+import { eq, and, isNull, getTableColumns, count } from 'drizzle-orm';
 import { alias } from 'drizzle-orm/pg-core';
 
 type commentWithPost = {
@@ -13,15 +13,21 @@ type commentWithPost = {
 export const load =  async({params})=>{
     const username = params.username
            
+    const user = await dbClient.select().from(usersTable).where(eq(usersTable.username, username))
+    
+    if(!user){
+        return{404: "User not found"}
+    }
 
     // using an alias to differentiate between the userstable join with the post and the comment
+    
     const commentAuthor = alias(usersTable, "commentAuthor");
     const postAuthor = alias(usersTable, "postAuthor");
     const userComments = await dbClient
     .select({
         comment: {
             ...getTableColumns(commentsTable),
-            imageUrl: commentAuthor.profilePictureUrl
+            imageUrl: commentAuthor.profilePictureUrl,
         },
         post: {
             ...getTableColumns(postsTable),
@@ -31,7 +37,7 @@ export const load =  async({params})=>{
     .from(commentsTable)
     .where(
         and(
-            eq(commentsTable.author, username),
+            eq(commentsTable.author, user[0].id),
             isNull(commentsTable.parentCommentId)
         )
     )
@@ -39,8 +45,9 @@ export const load =  async({params})=>{
     .leftJoin(commentAuthor, eq(commentsTable.author, commentAuthor.username))
     .leftJoin(postAuthor, eq(postsTable.author, postAuthor.username));
 
+    console.log(userComments)
 
     return {
-        userComments: userComments as commentWithPost
+        userComments
     }
 }
--- src/routes/(app)/users/[username]/comments/+page.svelte
+++ src/routes/(app)/users/[username]/comments/+page.svelte
@@ -9,7 +9,7 @@
     <div class="comments-container">
         {#each data.userComments as comment}
             <div>
-                <Post post={comment.post }/>
+                <Post post={comment.post}/>
                 <div style="margin-left: 2rem; margin-bottom: 1rem;">
                     <Comment postId={comment.post.id} comment={comment.comment}/>
                 </div>
